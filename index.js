62ff7f481464169f4a3f3a5a77f5b1397f6a29bd
Jolldev <69489813+Jolldev@users.noreply.github.com>
2020-11-29 15:19:40 -0600

    Add files via upload
    Carousel works on mobile but too tall on pc. JS is commented out

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 
 
-const carouselInner = document.querySelector('.carousel-inner');
-const carouselImg = document.querySelector('.carousel-img');
-const topSection = document.querySelector('.top-section');
-const marble = document.querySelector('.marble-container');
-const marbleHeight = marble.offsetHeight;
+// const carouselInner = document.querySelector('.carousel-inner');
+// const carouselImg = document.querySelector('.carousel-img');
+// const topSection = document.querySelector('.top-section');
+// const marble = document.querySelector('.marble-container');
+// const marbleHeight = marble.offsetHeight;
 
 
-function setTest(){
+// function setTest(){
    
 
-    carouselImg.style.height= window.innerHeight-marbleHeight +"px";
-    // image.style.height = test.offsetHeight - alignHeight + "px";
-    // console.log(test.offsetHeight);
-    // console.log(alignHeight);
-    // console.log(test.offsetHeight - alignHeight + "px");
-    // console.log(image.style.height);
-}
-setTest();
+//     carouselImg.style.height= window.innerHeight-marbleHeight +"px";
+//     // image.style.height = test.offsetHeight - alignHeight + "px";
+//     // console.log(test.offsetHeight);
+//     // console.log(alignHeight);
+//     // console.log(test.offsetHeight - alignHeight + "px");
+//     // console.log(image.style.height);
+// }
+// setTest();
  
