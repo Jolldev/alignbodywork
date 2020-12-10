23d1e571a82ff8ca8ee4dd110f911d7086f7fc07
Jolldev <69489813+Jolldev@users.noreply.github.com>
2020-12-10 05:00:35 -0600

    Add files via upload
    Added testimonial carousel, added custom photo main carousel, lots of responsive margin/padding updating

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const logo = document.querySelector('.logo-nav');
 const logoHeight = logo.offsetHeight;
 
 const carouselArray = Array.from(carouselImg);
-console.log(carouselArray);
+// console.log(carouselArray);
 
 function setHeight() {
 carouselArray.forEach(element => {
@@ -25,11 +25,24 @@ function setHeightHalf() {
     });
 }
 
-// setHeight(carouselArray);
+
  
-if (window.innerWidth >= 768) { 
+if (window.innerWidth > 1024) { 
     setHeight();
 } else {
     setHeightHalf();
 }
+
+// var cells = document.querySelectorAll('.testimonial-cell');
+// cells = Array.from(cells);
+// cells.forEach(element => {
+//     console.log(element.offsetHeight);
+// })
+
+var maxHeight = 0;
+$('.testimonial-cell').each(function () {
+    maxHeight = Math.max(maxHeight, $(this).outerHeight());
+});
+$('.quote-1, .quote-2, .quote-3').css({ height: maxHeight + 'px' });
+
 
