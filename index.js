2046d839060f4901abb5ea6384461306c450937a
Jolldev <69489813+Jolldev@users.noreply.github.com>
2021-02-26 16:14:48 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,47 +1,56 @@
 // Sets height of main page landscape oriented carousel images to equal the space between
 // the bottom of navbar and bottom of window
-const landscapeImg = document.querySelectorAll(".landscape-slide-img");
-const landscapeImgArray = Array.from(landscapeImg);
 const logo = document.querySelector('.logo-nav');
-const logoHeight = logo.offsetHeight;
 
 function setHeight() {
-    landscapeImgArray.forEach(element => {
-        element.style.height = window.innerHeight - logoHeight + "px";
+    const logoHeight = logo.offsetHeight;
+    $('.landscape-slide-img').each(function () {
+        this.style.height = window.innerHeight - logoHeight + "px";
     });
 }
 
-var aspectRatio = window.innerWidth/window.innerHeight
+// var aspectRatio = window.innerWidth / window.innerHeight
 
 function windowSize() {
-    if (aspectRatio > 1.5 && window.innerHeight > 450) {
+    if (window.innerWidth / window.innerHeight > 1.5 && window.innerHeight > 460) {
         setHeight();
     } else {
-        // setHeightHalf();
         return;
+        // portraitHeight();
     }
 }
 
+// Sets all main page carousel images (screen in portrait orientation) to the same height (Don't need if
+// using same aspect ratios)
 
-// Sets all main page carousel images (screen in portrait orientation) to the same height
-var maxHeightSlide = 200;
-function portraitHeight(){
-$('.portrait-slide-img').each(function () {
-    maxHeightSlide = Math.max(maxHeightSlide, $(this).outerHeight());
-    return maxHeightSlide;
-});
-$('.slide-1, .slide-2, .slide-3, .slide-4').css({height: maxHeightSlide + 'px' });
-// $('.slide-1, .slide-2, .slide-3, .slide-4').css({ maxHeight: "100%" });
-};
 
-window.addEventListener('load', windowSize);
-window.addEventListener('load', portraitHeight);
+// function portraitHeight() {
+//     var maxHeightSlide = 100;
+//     $('.portrait-slide-img').each(function () {
+//         $(this).css({ width: '100%' });
+//         $(this).css({ height: 'auto' });
+//         maxHeightSlide = Math.max(maxHeightSlide, $(this).outerHeight());
+//         return maxHeightSlide;
+//     });
+
+//     $('.portrait-slide-img').each(function () {
+//         $(this).css({ width: '100%' });
+//         $(this).css({ height: maxHeightSlide + 'px' });
+//     });
+    
+// };
+
+
+// window.addEventListener('load', windowSize);
 window.addEventListener('resize', windowSize);
+window.addEventListener("orientationchange", windowSize);
+// window.addEventListener('load', portraitHeight);
+// window.addEventListener('resize', windowSize);
 
 
 // Makes all testimonial text boxes on main page same height by matching heighest
 function carouselNormalization() {
-    var items = $('#carouselTestimonials .carousel-item'), //grab all slides
+    var items = $('.testimonial-cell'), //grab all slides
         heights = [], //create empty array to store height values
         tallest; //create variable to make note of the tallest slide
 
@@ -52,20 +61,36 @@ function carouselNormalization() {
             });
             tallest = Math.max.apply(null, heights); //cache largest value
             items.each(function () {
-                $('.testimonial-cell').css('min-height', tallest + 'px');
+                $('.testimonial-cell').css('height', tallest + 'px');
             });
         };
         normalizeHeights();
 
-        // $(window).on('resize orientationchange', function () {
-        //     tallest = 0, heights.length = 0; //reset vars
-        //     items.each(function () {
-        //         $(this).css('min-height', '0'); //reset min-height
-        //     });
-        //     normalizeHeights(); //run it again 
-        // });
+        $(window).on('orientationchange', function () {
+            $('.carousel').carousel(0); //resets carousel to first (tallest) slide
+            tallest = 0, heights.length = 0; //reset vars
+            items.each(function () {
+                $(this).css({ height: '0' });
+                $(this).css('min-height', '0'); //reset min-height
+                $(this).css({height: 'auto'});
+            });
+            carouselNormalization(); //run it again 
+        });
+
+        $(window).on('resize', function () {
+            tallest = 0, heights.length = 0; //reset vars
+            $('.carousel').carousel(0); //resets carousel to first (tallest) slide
+            items.each(function () {
+                $(this).css({ height: '0' });
+                $(this).css('min-height', '0'); //reset min-height
+                $(this).css({ height: 'auto' });
+            });
+            carouselNormalization(); //run it again 
+        });
     }
 }
+
+
 
 /**
  * Wait until all the assets have been loaded so a maximum height 
@@ -73,5 +98,37 @@ function carouselNormalization() {
  */
 window.onload = function () {
     carouselNormalization();
-    windowSize();
 }
+
+document.addEventListener("DOMContentLoaded", windowSize);
+
+// Lazy load images on main page
+document.addEventListener("DOMContentLoaded", function () {
+    var lazyloadImages = document.querySelectorAll(".lazy");
+    var lazyloadThrottleTimeout;
+
+    function lazyload() {
+        if (lazyloadThrottleTimeout) {
+            clearTimeout(lazyloadThrottleTimeout);
+        }
+
+        lazyloadThrottleTimeout = setTimeout(function () {
+            var scrollTop = window.pageYOffset;
+            for (var i = 0; i < lazyloadImages.length; i++) {
+                if (lazyloadImages[i].offsetTop < (window.innerHeight + scrollTop)) {
+                    lazyloadImages[i].src = lazyloadImages[i].dataset.src;
+                    lazyloadImages[i].classList.remove('lazy');
+                }
+            }
+            if (lazyloadImages.length == 0) {
+                document.removeEventListener("scroll", lazyload);
+                window.removeEventListener("resize", lazyload);
+                window.removeEventListener("orientationChange", lazyload);
+            }
+        }, 20);
+    }
+
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+});
